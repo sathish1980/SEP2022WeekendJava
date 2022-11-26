12cb492c06550d7a4bf2c8a114cdf80133766de3
sathish1980 <kumar.sathish189@gmail.com>
2022-11-26 09:04:03 +0530

    mergeconflict

--- src/main/java/weekend/SEP2022WeekenedJAVA/branchconcept.java
+++ src/main/java/weekend/SEP2022WeekenedJAVA/branchconcept.java
@@ -1,6 +1,11 @@
 package weekend.SEP2022WeekenedJAVA;
 
 public class branchconcept {
+	
+	public void sathish()
+	{
+		System.out.println("this is sathish");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
