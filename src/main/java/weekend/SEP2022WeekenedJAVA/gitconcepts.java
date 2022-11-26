7430fe89e77a4817acb2431ae2ea78b5fa58570d
sathish1980 <kumar.sathish189@gmail.com>
2022-11-26 08:44:07 +0530

    chnages  done

--- src/main/java/weekend/SEP2022WeekenedJAVA/gitconcepts.java
+++ src/main/java/weekend/SEP2022WeekenedJAVA/gitconcepts.java
@@ -1,10 +1,16 @@
 package weekend.SEP2022WeekenedJAVA;
 
 public class gitconcepts {
+	
+	public void add()
+	{
+		System.out.println("this is to check the data");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		gitconcepts g = new gitconcepts();
+		g.add();
 	}
 
 }
