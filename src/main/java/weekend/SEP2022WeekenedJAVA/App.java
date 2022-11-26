e36f07a7d8d676e00f77d86e9904358868e0f059
sathish1980 <kumar.sathish189@gmail.com>
2022-11-26 08:28:57 +0530

    new selenium project created

---
+++ src/main/java/weekend/SEP2022WeekenedJAVA/App.java
@@ -0,0 +1,13 @@
+package weekend.SEP2022WeekenedJAVA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
